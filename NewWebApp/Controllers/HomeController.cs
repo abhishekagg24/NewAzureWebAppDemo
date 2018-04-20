a7f7d1b7956a6fbfdce12db83201af5935541031
abhishekagg24 <abhishekagg24@gmail.com>
2018-04-20 21:26:09 +0100

    updating home page

--- NewWebApp/Controllers/HomeController.cs
+++ NewWebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +11,10 @@ namespace NewWebApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var env  = ConfigurationManager.AppSettings.Get("Env");
+
+            return Content(env);
+            //return View();
         }
 
         public ActionResult About()
